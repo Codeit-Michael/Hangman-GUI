b663320e94af13d45f283a1d74f92ea18d8c5acf
Michael Maranan <michaelmaranan030@gmail.com>
2023-02-18 17:51:32 +0800

    update game

--- main.py
+++ main.py
@@ -1,54 +1,48 @@
 import pygame
-from pygame.locals import *
 
 pygame.init()
 pygame.font.init()
 
 screen = pygame.display.set_mode((400, 500))
-screen.fill((125,0,0))
-pygame.display.update()
+pygame.display.set_caption("Hangman")
 
-def gallow(gallow_color=(0,0,0)):
-    # gallow pieces
-    stand = pygame.draw.rect(screen, gallow_color, pygame.Rect(75, 280, 120, 10))
-    body = pygame.draw.rect(screen, gallow_color, pygame.Rect(128, 40, 10, 240))
-    hanger = pygame.draw.rect(screen, gallow_color, pygame.Rect(128, 40, 80, 10))
-    rope = pygame.draw.rect(screen, gallow_color, pygame.Rect(205, 40,10, 30))
-    pygame.display.update()
 
-def man_pieces(body_color=(255,253,175)):
-    head = pygame.draw.circle(screen, body_color, [210, 85], 20, 0)
-    body = pygame.draw.rect(screen, body_color, pygame.Rect(206, 105, 8, 45))
-    r_arm = pygame.draw.line(screen, body_color, [183, 149], [200, 107], 6)
-    l_arm = pygame.draw.line(screen, body_color, [231, 149], [218, 107], 6)
-    r_leg = pygame.draw.line(screen, body_color, [189, 198], [208, 148], 6)
-    l_leg = pygame.draw.line(screen, body_color, [224, 198], [210, 148], 6)
+class Hangman():
+    def __init__(self):
+        self.running = True
+        self.FPS = pygame.time.Clock()
+        self.gallow_color = (0,0,0)
+        self.body_color = (255,253,175)
 
-    pygame.display.update()
+    def gallow(self):
+        stand = pygame.draw.rect(screen, self.gallow_color, pygame.Rect(75, 280, 120, 10))
+        body = pygame.draw.rect(screen, self.gallow_color, pygame.Rect(128, 40, 10, 240))
+        hanger = pygame.draw.rect(screen, self.gallow_color, pygame.Rect(128, 40, 80, 10))
+        rope = pygame.draw.rect(screen, self.gallow_color, pygame.Rect(205, 40,10, 30))
 
-guessed_word = "Guessed word: b*st"
-wrong_guesses = 'Wrong guesses: r, a, i'
-enter_guess = "Enter guess: "
+    def man_pieces(self):
+        head = pygame.draw.circle(screen, self.body_color, [210, 85], 20, 0)
+        body = pygame.draw.rect(screen, self.body_color, pygame.Rect(206, 105, 8, 45))
+        r_arm = pygame.draw.line(screen, self.body_color, [183, 149], [200, 107], 6)
+        l_arm = pygame.draw.line(screen, self.body_color, [231, 149], [218, 107], 6)
+        r_leg = pygame.draw.line(screen, self.body_color, [189, 198], [208, 148], 6)
+        l_leg = pygame.draw.line(screen, self.body_color, [224, 198], [210, 148], 6)
 
-running = True
-while running:
-    clock = pygame.time.Clock()
-    clock.tick(60)
-    gallow()
-    man_pieces()
 
-    font = pygame.font.SysFont("Arial", 25)
+    def main(self):
+        screen.fill((125, 0, 0))
+        self.gallow()
+        while self.running:
+            for self.event in pygame.event.get():
+                if self.event.type == pygame.QUIT:
+                    self.running = False
+                elif self.event.type == pygame.KEYDOWN:
+                    self.man_pieces()
 
-    text1 = font.render(guessed_word, True, (0,255,0))
-    screen.blit(text1,(75,330))    
-    text2 = font.render(wrong_guesses, True, (0,0,255))
-    screen.blit(text2,(75,360))    
-    text3 = font.render(enter_guess, True, (255,0,255))
-    screen.blit(text3,(75,390))
+            pygame.display.update()
+            self.FPS.tick(60)
 
-    pygame.display.update()
+        pygame.quit()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
+h = Hangman()
+h.main()
